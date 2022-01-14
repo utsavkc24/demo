4d47851704015c1b2683fbe6e25192449bd25a09
Utsav Kumar <utsavkc24@gmail.com>
2022-01-14 20:12:48 +0530

    How to do basic JPQL

---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/JPQLTest.java
@@ -0,0 +1,52 @@
+package com.in28minutes.jpa.hibernate.demo.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import com.in28minutes.jpa.hibernate.demo.DemoApplication;
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+class JPQLTest {
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		LOGGER.info("Test is running");
+	}
+
+	@Test
+	void jpql_basic() {
+		Query query = em.createQuery("select c from Course c");
+		List resultList = query.getResultList();
+		LOGGER.info("select c from Course -> {}", resultList);
+	}
+
+	@Test
+	void jpql_typed() {
+		TypedQuery<Course> query = em.createQuery("select c from Course c", Course.class);
+		List<Course> resultList = query.getResultList();
+		LOGGER.info("TYPED select c from Course -> {}", resultList);
+	}
+
+	@Test
+	void jpql_where() {
+		TypedQuery<Course> query = em.createQuery("select c from Course c where name = 'History'", Course.class);
+		List<Course> resultList = query.getResultList();
+		LOGGER.info("select c from Course c where name = 'History' -> {}", resultList);
+	}
+
+}
