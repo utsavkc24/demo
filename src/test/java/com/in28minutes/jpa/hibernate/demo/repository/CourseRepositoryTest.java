b1c4765b3ab95a03d45a186fc7ce8fd20f2bc6e7
Utsav Kumar <utsavkc24@gmail.com>
2022-01-22 14:33:34 +0530

    difference between OneToMany and ManyToOne

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -3,8 +3,11 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import javax.persistence.EntityManager;
+
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Review;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,12 +15,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = DemoApplication.class)
 class CourseRepositoryTest {
 	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	CourseRepository courseRepository;
+
+	@Autowired
+	EntityManager entityManager;
 
 	@Test
 	void contextLoads() {
@@ -50,5 +57,19 @@ class CourseRepositoryTest {
 		// check
 		Course course1 = courseRepository.findById(10001L);
 		assertEquals("History - Updated", course1.getName());
+	}
+
+	@Test
+	@Transactional
+	public void retrieveReviewFromCourse() {
+		Course course = courseRepository.findById(10001L);
+		LOGGER.info("{} ", course.getReviews());
+	}
+
+	@Test
+	// @Transactional
+	public void retrieveCourseFromReview() {
+		Review review = entityManager.find(Review.class, 50001L);
+		LOGGER.info("{} ", review.getCourse());
 	}
 }
