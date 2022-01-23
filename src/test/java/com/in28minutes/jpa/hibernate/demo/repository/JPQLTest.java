70f3141905b315d4d4eb1fb54bd778d37e7319f8
Utsav Kumar <utsavkc24@gmail.com>
2022-01-23 17:03:38 +0530

    Basic useful JPQL Queries

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/JPQLTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/JPQLTest.java
@@ -8,6 +8,7 @@ import javax.persistence.TypedQuery;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Student;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -45,6 +46,39 @@ class JPQLTest {
 		TypedQuery<Course> query = em.createNamedQuery("get_all_courses_with_where_clause", Course.class);
 		List<Course> resultList = query.getResultList();
 		LOGGER.info("select c from Course c where name = 'History' -> {}", resultList);
+	}
+
+	// Course without student
+	@Test
+	void jpql_course_without_student() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c where c.students is empty", Course.class);
+		List<Course> resultList = query.getResultList();
+		LOGGER.info("select c from course c where c.students is empty -> {}", resultList);
+	}
+
+	// Course with atleast 2 student
+	@Test
+	void jpql_course_with_atleast_2_student() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c where size(c.students) >= 2", Course.class);
+		List<Course> resultList = query.getResultList();
+		LOGGER.info("Select c from Course c where size(c.students) >= 2 -> {}", resultList);
+	}
+
+	// Course ordered by student(By default = ascending)
+	@Test
+	void jpql_courses_ordered_by_student() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c order by size(c.students)", Course.class);
+		List<Course> resultList = query.getResultList();
+		LOGGER.info("Select c from Course c order by size(c.students) -> {}", resultList);
+	}
+
+	// Student whose passport contain '123'
+	@Test
+	void jpql_student_whose_passport_in_a_pattern() {
+		TypedQuery<Student> query = em.createQuery("Select s from Student s where s.passport.number like '%123%'",
+				Student.class);
+		List<Student> resultList = query.getResultList();
+		LOGGER.info("Select s from Student s where s.passport.number like '%1234%' -> {}", resultList);
 	}
 
 }
